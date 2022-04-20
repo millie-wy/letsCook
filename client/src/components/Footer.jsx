0708358654da8bc36f87ac77091c1df41b34e2ee
Millie Cheung <cheungmillie9@gmail.com>
2022-04-20 14:47:41 +0200

    add jsconfig for auto imports, imported react

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const Footer = () => {
   return <div>Footer</div>;
 };
--- client/src/components/pages/StartPage.jsx
+++ client/src/components/pages/StartPage.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const StartPage = () => {
   return <div>StartPage</div>;
 };
