9df64c4e935a8904503d1e0a51ef88f1fde3212c
William Saar <williamsaar@outlook.com>
2022-04-20 12:51:31 +0200

    correct colors and layout

--- client/src/components/pages/StartPage.jsx
+++ client/src/components/pages/StartPage.jsx
@@ -1,22 +1,71 @@
 import { Box, Button, Container, Typography } from "@mui/material";
+import { textAlign } from "@mui/system";
 import React from "react";
 
 const StartPage = () => {
   return (
-    <main style={{ backgroundColor: "#B6D5D5" }}>
+    <main style={{ backgroundColor: "#F1F8F6", height: "calc(100vh - 3rem)" }}>
       <Container>
-        <Box sx={{ display: "flex", flexDirection: "column" }}>
-          <Box sx={{ display: "flex", alignItems: "center" }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
             <Box>
               <Box>
-                <Typography sx={{ color: "#2E4739" }}>Looking for</Typography>
-                <Typography sx={{ color: "#E5C687" }}>delicious</Typography>
-                <Typography sx={{ color: "#2E4739" }}>recipes?</Typography>
+                <Typography
+                  sx={{
+                    color: "#2E4739",
+                    fontWeight: "900",
+                    fontSize: "3rem",
+                    transform: "scaleY(1.1)",
+                  }}
+                >
+                  Looking for
+                </Typography>
+
+                <Typography
+                  sx={{
+                    color: "#E5C687",
+                    fontWeight: "900",
+                    fontStyle: "normal",
+                    fontSize: "5rem",
+                    lineHeight: "1.2",
+                    transform: "scaleY(1.1)",
+                  }}
+                >
+                  delicious
+                </Typography>
+                <Typography
+                  sx={{
+                    color: "#2E4739",
+                    fontWeight: "900",
+                    fontSize: "3rem",
+                    transform: "scaleY(1.1)",
+                  }}
+                >
+                  recipes?
+                </Typography>
               </Box>
               <Box>
-                <Typography sx={{ color: "#989C9C" }}>
-                  Whether you want a meal to impress the royal family of Monaco
-                  or a quick meal for monday after work, we've got you!
+                <Typography
+                  sx={{
+                    color: "#989C9C",
+                    fontSize: "1.2rem",
+                    paddingTop: "1rem",
+                  }}
+                >
+                  Whether you want a meal to impress the <br /> royal family of
+                  Monaco or a quick meal for <br /> monday after work, we've got
+                  you!
                 </Typography>
               </Box>
             </Box>
@@ -29,7 +78,7 @@ const StartPage = () => {
               />
             </Box>
           </Box>
-          <Box>
+          <Box sx={{ textAlign: "center", paddingTop: "5rem" }}>
             <button
               style={{
                 backgroundColor: "#0B814A",
