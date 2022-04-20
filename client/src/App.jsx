d3319e621f578152f3801f0049999cb63536cb5a
Millie Cheung <cheungmillie9@gmail.com>
2022-04-20 18:14:47 +0200

    added routes and corrected links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import RecipeStartPage from "./components/pages/RecipeStart/RecipeStartPage";
 import SearchResult from "./components/pages/RecipeStart/SearchResult";
+import RecipeDetailPage from "./components/pages/RecipeStart/RecipeDetailPage";
 import SignIn from "./components/pages/SignInUp/SignIn";
 import SignUp from "./components/pages/SignInUp/SignUp";
 import StartPage from "./components/pages/StartPage";
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<StartPage />} />
           <Route path="/start" element={<RecipeStartPage />} />
+          <Route path="/recipe/:id" element={<RecipeDetailPage />} />
           <Route path="/search" element={<SearchResult />} />
           <Route path="/login" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
