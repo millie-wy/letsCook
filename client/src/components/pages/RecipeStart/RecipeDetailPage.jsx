c2b71f84ada902742bded32bdbddf60d1e056680
Millie Cheung <cheungmillie9@gmail.com>
2022-04-20 18:16:05 +0200

    added basic code for recipe detail page for later use

---
+++ client/src/components/pages/RecipeStart/RecipeDetailPage.jsx
@@ -0,0 +1,27 @@
+import { Box, Container } from "@mui/material";
+import React from "react";
+import { useParams } from "react-router-dom";
+// @ts-ignore
+import data from "../../../data.json";
+
+const RecipeDetailPage = () => {
+  const params = useParams();
+  const recipe = data.find((recipe) => recipe.id === params?.id);
+
+  return (
+    <Container sx={{ minHeight: "calc(100vh - 8rem)", mt: "2rem" }}>
+      Recipe Detail Page - this page is for:
+      <h3>{recipe.title}</h3>
+      <Box
+        component="img"
+        height="180px"
+        src={recipe.image}
+        title={recipe.title}
+        sx={{ borderRadius: 8 }}
+      />
+      <p>{recipe.description}</p>
+    </Container>
+  );
+};
+
+export default RecipeDetailPage;
