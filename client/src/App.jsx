3e2c1100d25a3264096a8ac77461168505a080e6
Millie Cheung <cheungmillie9@gmail.com>
2022-04-20 16:30:15 +0200

    layout for searchResult (cards not inserted)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import RecipeStartPage from "./components/pages/RecipeStart/RecipeStartPage";
+import SearchResult from "./components/pages/RecipeStart/SearchResult";
 import SignIn from "./components/pages/SignInUp/SignIn";
 import SignUp from "./components/pages/SignInUp/SignUp";
 import StartPage from "./components/pages/StartPage";
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<StartPage />} />
           <Route path="/start" element={<RecipeStartPage />} />
+          <Route path="/search" element={<SearchResult />} />
           <Route path="/login" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
         </Route>
