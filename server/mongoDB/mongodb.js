6f5db8b99f6131d2d0524a82336d6c82cfe1e499
William Saar <williamsaar@outlook.com>
2022-04-21 11:42:15 +0200

    removed unnecessary mongodb folder

--- server/mongoDB/mongodb.js
+++
@@ -1,8 +0,0 @@
-import { MongoClient } from "mongodb";
-let url = "mongodb://localhost:27017/test";
-
-MongoClient.connect(url, (err, db) => {
-  if (err) throw err;
-  console.log("Database created");
-  db.close();
-});
