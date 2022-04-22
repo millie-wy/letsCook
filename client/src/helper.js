9e2ba3f4fcb96b511fd50d0053fbd8b40350d90e
Millie Cheung <cheungmillie9@gmail.com>
2022-04-22 15:24:50 +0200

    created for shared/exported functions

---
+++ client/src/helper.js
@@ -0,0 +1,10 @@
+export const makeRequest = async (url, method, body) => {
+  let response = await fetch(url, {
+    method,
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return await response.json();
+};
