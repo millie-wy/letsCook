16d2a6d619f709316aeb920eed7db9877ab26685
Millie Cheung <cheungmillie9@gmail.com>
2022-04-27 09:52:09 +0200

    changed author to object instead of array

--- server/models/recipe.model.js
+++ server/models/recipe.model.js
@@ -9,19 +9,17 @@ const schema = new mongoose.Schema({
   ingredients: [{ type: String, required: true }],
   direction: [{ type: String, required: true }],
   tags: [{ type: String }],
-  author: [
-    {
-      id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "user",
-        required: true,
-      },
-      email: { type: String },
-      firstName: { type: String },
-      lastName: { type: String },
-      role: { type: String },
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
     },
-  ],
+    email: { type: String },
+    firstName: { type: String },
+    lastName: { type: String },
+    role: { type: String },
+  },
   comments: [
     {
       author: { type: String },
