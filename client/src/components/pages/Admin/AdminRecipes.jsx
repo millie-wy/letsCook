e1cbfd58161cbe2a39177c70edf521496bd351d1
William Saar <williamsaar@outlook.com>
2022-04-27 17:19:42 +0200

    admin recipe page done

--- client/src/components/pages/Admin/AdminRecipes.jsx
+++ client/src/components/pages/Admin/AdminRecipes.jsx
@@ -1,99 +1,161 @@
 import { Add } from "@mui/icons-material";
-import { Box, Button, Container, Link, Typography } from "@mui/material";
-import React from "react";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Container,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import comment from "../../../assets/logoAndIcons/comment.svg";
+import { makeRequest } from "../../../helper";
 
 const AdminRecipes = () => {
-  return (
+  const [recipes, setRecipes] = useState([]);
+  const matches = useMediaQuery("(max-width:500px)");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let response = await makeRequest("/api/recipes", "GET");
+      setRecipes(response);
+    };
+    fetchData();
+  }, []);
+
+  const getAvgRating = (comments) => {
+    let rated = [];
+    comments.map((comment) => rated.push(comment.rated));
+    let avg = rated.reduce((a, b) => a + b, 0) / comments.length || 0;
+    return avg.toFixed(1);
+  };
+
+  return recipes.length < 1 ? (
+    <Container sx={{ height: "calc(100vh - 8rem)", mt: "2rem" }}>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100%",
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    </Container>
+  ) : (
     <Container sx={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+      <Typography
+        variant="body1"
+        sx={{
+          fontFamily: "Poppins",
+          lineHeight: 1.5,
+          fontWeight: 600,
+          mx: ".5rem",
+        }}
+      >
+        Here you can see all recipes, Click "To recipe" to{" "}
+        <span style={{ color: "#0B814A" }}>edit</span> or{" "}
+        <span style={{ color: "#FF5858" }}>delete</span> them.
+      </Typography>
       {/* profile recipe card - delete the second card and fetch and map to be used here */}
-      <Container
-        sx={{
-          background: "white",
-          borderRadius: 5,
-          py: "1.5rem",
-          w: 1,
-        }}
-      >
-        <Box
+      {recipes.map((recipe) => (
+        <Container
+          key={recipe._id}
           sx={{
-            display: "flex",
-            flexDirection: "row",
-            gap: "2rem",
+            background: "white",
+            borderRadius: 5,
+            py: "1.5rem",
+            w: 1,
           }}
         >
           <Box
-            width="70%"
             sx={{
               display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-between",
+              flexDirection: "row",
+              gap: "2rem",
             }}
           >
-            <Box>
-              <Typography
-                variant="body1"
-                sx={{
-                  fontFamily: "Poppins",
-                  fontWeight: 600,
-                  lineHeight: 1,
-                  mb: ".5rem",
-                }}
-              >
-                Pizza Pepperoni
-              </Typography>
-              <Typography
-                variant="body2"
-                color="#989C9C"
-                sx={{
-                  fontFamily: "Poppins",
-                  mb: ".5rem",
-                }}
-              >
-                Pepperoni pizza is a American variety of salami, made from cured
-                pork and beef seasoned with paprika or other chili pepper.
-              </Typography>
-            </Box>
             <Box
-              width="95%"
+              width="100%"
               sx={{
                 display: "flex",
-                flexDirection: "row",
+                flexDirection: "column",
                 justifyContent: "space-between",
               }}
             >
-              <Link
-                to="/"
-                //  to={`/recipe/${recipe.title.replaceAll(" ", "-")}`} // to be fixed
-                style={{ textDecoration: "none" }}
-              >
-                <Button
-                  variant="outlined"
-                  size="small"
+              <Box>
+                <Typography
+                  variant="body1"
                   sx={{
                     fontFamily: "Poppins",
-                    letterSpacing: "none",
-                    fontSize: ".7rem",
-                    fontWeight: "600",
-                    bgcolor: "white",
-                    color: "#0B814A",
-                    border: "1.2px solid #0B814A",
-                    textTransform: "capitalize",
-                    borderRadius: 10,
-                    boxShadow: "none",
+                    fontWeight: 600,
+                    lineHeight: 1,
+                    mb: ".5rem",
                   }}
                 >
-                  To recipe
-                </Button>
-              </Link>
+                  {recipe.title}
+                </Typography>
+                {matches && (
+                  <Box
+                    component="img"
+                    src={recipe.image}
+                    alt={recipe.title}
+                    sx={{
+                      objectFit: "cover",
+                      borderRadius: "20px",
+                      height: { xs: "100px", sm: "150px", md: "150px" },
+                      width: { xs: "70%", sm: "100%", md: "100%" },
+                    }}
+                  />
+                )}
+                <Typography
+                  variant="body2"
+                  color="#989C9C"
+                  sx={{
+                    fontFamily: "Poppins",
+                    mb: ".5rem",
+                  }}
+                >
+                  {recipe.description}
+                </Typography>
+              </Box>
               <Box
+                width="95%"
                 sx={{
                   display: "flex",
                   flexDirection: "row",
-                  gap: ".3rem",
-                  placeItems: "center",
+                  justifyContent: "space-between",
                 }}
               >
+                <Link
+                  to={`/recipe/${recipe.title.replaceAll(" ", "-")}`}
+                  state={{
+                    id: recipe._id,
+                    rating: getAvgRating(recipe.comments),
+                  }}
+                  style={{ textDecoration: "none" }}
+                >
+                  <Button
+                    variant="outlined"
+                    size="small"
+                    sx={{
+                      fontFamily: "Poppins",
+                      letterSpacing: "none",
+                      fontSize: ".7rem",
+                      fontWeight: "600",
+                      bgcolor: "white",
+                      color: "#0B814A",
+                      border: "1.2px solid #0B814A",
+                      textTransform: "capitalize",
+                      borderRadius: 10,
+                      boxShadow: "none",
+                    }}
+                  >
+                    To recipe
+                  </Button>
+                </Link>
                 <Box
                   sx={{
                     display: "flex",
@@ -101,217 +163,63 @@ const AdminRecipes = () => {
                     gap: ".3rem",
                     placeItems: "center",
                   }}
-                >
-                  <Box
-                    component="img"
-                    style={{ height: "18px" }}
-                    src={comment}
-                    alt="comment"
-                  />
-                  <Typography
-                    sx={{
-                      fontFamily: "Poppins",
-                      fontWeight: 600,
-                      color: "#E5C687",
-                      fontSize: ".9rem",
-                      margin: "auto",
-                    }}
-                    gutterBottom
-                    variant="body2"
-                  >
-                    123
-                    {/* {recipe.comments.length} */}
-                  </Typography>
-                </Box>
+                ></Box>
               </Box>
-            </Box>
-          </Box>
-          <Box
-            width="30%"
-            minWidth="140px"
-            sx={{
-              borderRadius: "20px",
-            }}
-          >
-            <Box
-              height="150px"
-              width="100%"
-              component="img"
-              src="https://lh3.googleusercontent.com/cKFMscy0nPuWC-EiEmEiuY17SfvIALbfiACen-SArCMLBMhrIRx753rXj4IrM07xItTDZMgR2k0-5VjZ0gw8PZxQEwiQrWhx4C39=s640-c-rw-v1-e365"
-              alt="name of the dish"
-              sx={{
-                objectFit: "cover",
-                borderRadius: "20px",
-              }}
-            />
-          </Box>
-        </Box>
-      </Container>
-      <Container
-        sx={{
-          background: "white",
-          borderRadius: 5,
-          py: "1.5rem",
-          w: 1,
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            gap: "2rem",
-          }}
-        >
-          <Box
-            width="70%"
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-between",
-            }}
-          >
-            <Box>
-              <Typography
-                variant="body1"
-                sx={{
-                  fontFamily: "Poppins",
-                  fontWeight: 600,
-                  lineHeight: 1,
-                  mb: ".5rem",
-                }}
-              >
-                Pizza Pepperoni
-              </Typography>
-              <Typography
-                variant="body2"
-                color="#989C9C"
-                sx={{
-                  fontFamily: "Poppins",
-                  mb: ".5rem",
-                }}
-              >
-                Pepperoni pizza is a American variety of salami, made from cured
-                pork and beef seasoned with paprika or other chili pepper.
-              </Typography>
-            </Box>
-            <Box
-              width="95%"
-              sx={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-between",
-              }}
-            >
-              <Link
-                to="/"
-                //  to={`/recipe/${recipe.title.replaceAll(" ", "-")}`} // to be fixed
-                style={{ textDecoration: "none" }}
-              >
-                <Button
-                  variant="outlined"
-                  size="small"
-                  sx={{
-                    fontFamily: "Poppins",
-                    letterSpacing: "none",
-                    fontSize: ".7rem",
-                    fontWeight: "600",
-                    bgcolor: "white",
-                    color: "#0B814A",
-                    border: "1.2px solid #0B814A",
-                    textTransform: "capitalize",
-                    borderRadius: 10,
-                    boxShadow: "none",
-                  }}
-                >
-                  To recipe
-                </Button>
-              </Link>
+            </Box>{" "}
+            {!matches && (
               <Box
+                width="30%"
+                minWidth="140px"
                 sx={{
+                  borderRadius: "20px",
                   display: "flex",
-                  flexDirection: "row",
-                  gap: ".3rem",
-                  placeItems: "center",
+                  justifyContent: "center",
+                  alignItems: "center",
                 }}
               >
                 <Box
+                  component="img"
+                  src={recipe.image}
+                  alt={recipe.title}
                   sx={{
-                    display: "flex",
-                    flexDirection: "row",
-                    gap: ".3rem",
-                    placeItems: "center",
+                    objectFit: "cover",
+                    borderRadius: "20px",
+                    height: { xs: "100px", sm: "150px", md: "150px" },
+                    width: { xs: "70%", sm: "100%", md: "100%" },
                   }}
-                >
-                  <Box
-                    component="img"
-                    style={{ height: "18px" }}
-                    src={comment}
-                    alt="comment"
-                  />
-                  <Typography
-                    sx={{
-                      fontFamily: "Poppins",
-                      fontWeight: 600,
-                      color: "#E5C687",
-                      fontSize: ".9rem",
-                      margin: "auto",
-                    }}
-                    gutterBottom
-                    variant="body2"
-                  >
-                    123
-                    {/* {recipe.comments.length} */}
-                  </Typography>
-                </Box>
+                />
               </Box>
-            </Box>
+            )}
           </Box>
-          <Box
-            width="30%"
-            minWidth="140px"
-            sx={{
-              borderRadius: "20px",
-            }}
-          >
-            <Box
-              height="150px"
-              width="100%"
-              component="img"
-              src="https://lh3.googleusercontent.com/cKFMscy0nPuWC-EiEmEiuY17SfvIALbfiACen-SArCMLBMhrIRx753rXj4IrM07xItTDZMgR2k0-5VjZ0gw8PZxQEwiQrWhx4C39=s640-c-rw-v1-e365"
-              alt="name of the dish"
-              sx={{
-                objectFit: "cover",
-                borderRadius: "20px",
-              }}
-            />
-          </Box>
-        </Box>
-      </Container>
+        </Container>
+      ))}
 
       {/* create recipe button  */}
-      <Button
-        sx={{
-          backgroundColor: "#0B814A",
-          color: "#F1F8F6",
-          fontFamily: "Poppins",
-          borderRadius: "70px",
-          border: "none",
-          width: "fit-content",
-          fontSize: ".7rem",
-          marginBottom: "1rem",
-          px: "1rem",
-          transition: "all .15s ease-in-out",
-          textTransform: "none",
-          alignSelf: "end",
-          "&:hover": {
-            background: "#0AA35C",
-            transform: "scale(1.01)",
-          },
-        }}
-      >
-        <Add fontSize="small" sx={{ mr: ".2rem" }} />
-        Create new recipe
-      </Button>
+      <Link to={`/recipe/create`} style={{ textDecoration: "none" }}>
+        <Button
+          sx={{
+            backgroundColor: "#0B814A",
+            color: "#F1F8F6",
+            fontFamily: "Poppins",
+            borderRadius: "70px",
+            border: "none",
+            width: "fit-content",
+            fontSize: ".7rem",
+            marginBottom: "1rem",
+            px: "1rem",
+            transition: "all .15s ease-in-out",
+            textTransform: "none",
+            alignSelf: "end",
+            "&:hover": {
+              background: "#0AA35C",
+              transform: "scale(1.01)",
+            },
+          }}
+        >
+          <Add fontSize="small" sx={{ mr: ".2rem" }} />
+          Create new recipe
+        </Button>
+      </Link>
     </Container>
   );
 };
