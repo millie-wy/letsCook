3f4caae41c681e4dec2645f2111d59dd75cf0a71
William Saar <williamsaar@outlook.com>
2022-04-27 19:39:52 +0200

    Button to delete user now works

--- client/src/components/pages/Admin/AdminUsers.jsx
+++ client/src/components/pages/Admin/AdminUsers.jsx
@@ -6,21 +6,32 @@ import {
 } from "@mui/icons-material";
 import {
   Avatar,
+  Backdrop,
   Box,
   Button,
   CircularProgress,
   Container,
+  Fade,
   IconButton,
+  Modal,
   Typography,
 } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useAccount } from "../../context/AccountContext";
 import { makeRequest } from "../../../helper";
+import { useNavigate } from "react-router-dom";
 
 const AdminUsers = () => {
   const { currentUser } = useAccount();
   const [isLoading, setIsLoading] = useState(true);
   const [users, setUsers] = useState();
+  const [isUpdating, setIsUpdating] = useState(false);
+  const navigate = useNavigate();
+
+  // for modal
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   useEffect(() => {
     if (currentUser === undefined) {
@@ -42,8 +53,17 @@ const AdminUsers = () => {
   const deleteAdmin = (id) => {
     console.log("User with id: " + id + " is no longer admin.");
   };
-  const deleteUser = (id) => {
-    console.log("User with id: " + id + " has now been deleted.");
+
+  const deleteUser = async (id, firstName, lastName) => {
+    setIsUpdating(true);
+    let response = await makeRequest(`/api/users/${id}`, "DELETE");
+    setTimeout(() => {
+      alert(`User: ${firstName} {$lastName} has been deleted`);
+
+      setIsUpdating(false);
+      handleClose();
+      navigate("/admin");
+    }, 1000);
   };
 
   return isLoading ? (
@@ -149,14 +169,106 @@ const AdminUsers = () => {
                   <AddModerator fontSize="large" sx={{ color: "#0B814A" }} />
                 </IconButton>
               )}
-              <IconButton
-                aria-label="delete"
-                size="large"
-                onClick={() => deleteUser(user._id)}
-              >
+              <IconButton aria-label="delete" size="large" onClick={handleOpen}>
                 <Delete fontSize="large" sx={{ color: "#FF5858" }} />
               </IconButton>
             </Box>
+            {!isUpdating ? (
+              <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                  timeout: 500,
+                }}
+              >
+                <Fade in={open}>
+                  <Box sx={modalStyle}>
+                    <Typography
+                      sx={{ fontFamily: "Poppins" }}
+                      id="transition-modal-title"
+                      variant="h6"
+                      component="h3"
+                    >
+                      Delete this user?
+                    </Typography>
+
+                    <Button
+                      sx={{
+                        borderRadius: "1rem",
+                        fontFamily: "Poppins",
+                        backgroundColor: "#FF5858",
+                        transition: "all .15s ease-in-out",
+                        textTransform: "capitalize",
+                        color: "#fff",
+                        "&:hover": {
+                          background: "#DC1919",
+                        },
+                      }}
+                      onClick={() =>
+                        deleteUser(user._id, user.firstName, user.lastName)
+                      }
+                    >
+                      Yes
+                    </Button>
+                    <Button
+                      sx={{
+                        borderRadius: "1rem",
+                        fontFamily: "Poppins",
+                        backgroundColor: "#C4C4C4",
+                        transition: "all .15s ease-in-out",
+                        textTransform: "capitalize",
+                        color: "#fff",
+                        "&:hover": {
+                          background: "#9C9999",
+                        },
+                      }}
+                      onClick={handleClose}
+                    >
+                      Cancel
+                    </Button>
+                  </Box>
+                </Fade>
+              </Modal>
+            ) : (
+              <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                  timeout: 500,
+                }}
+              >
+                <Fade in={open}>
+                  <Box sx={modalStyle}>
+                    <Typography
+                      sx={{ fontFamily: "Poppins" }}
+                      id="transition-modal-title"
+                      variant="h6"
+                      component="h3"
+                    >
+                      Deleting user...
+                    </Typography>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        height: "100%",
+                      }}
+                    >
+                      <CircularProgress />
+                    </Box>
+                  </Box>
+                </Fade>
+              </Modal>
+            )}
           </Container>
         ))}
       </Container>
@@ -185,6 +297,22 @@ const AdminUsers = () => {
       </Button>
     </Container>
   );
+};
+
+const modalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+  display: "flex",
+  flexDirection: "column",
+  rowGap: "1rem",
+  textAlign: "center",
 };
 
 export default AdminUsers;
